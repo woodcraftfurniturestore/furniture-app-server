94bad8259e9b6101206122884c4f1115063e49c8
HariKrish1122001 <harikrishnannexusdigitalia@gmail.com>
2025-11-07 16:34:01 +0530

    new mongo

--- config/db.js
+++ config/db.js
@@ -1,20 +1,4 @@
-// const mongoose = require('mongoose');
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log('✅ MongoDB Connected Successfully');
-//   } catch (error) {
-//     console.error('❌ MongoDB Connection Failed:', error.message);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
@@ -22,9 +6,9 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("✅ MongoDB Connected Successfully");
+    console.log('✅ MongoDB Connected Successfully');
   } catch (error) {
-    console.error("❌ MongoDB Connection Failed:", error.message);
+    console.error('❌ MongoDB Connection Failed:', error.message);
     process.exit(1);
   }
 };
